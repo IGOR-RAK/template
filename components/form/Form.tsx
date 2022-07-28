ca9cc562de3dbf3502b7a1c1b26c5f07b6797880
Igor Rak <igorrakfed@gmail.com>
2022-07-28 22:01:45 +0200

    Add form styles enlist fix problem 2

--- components/form/Form.tsx
+++ components/form/Form.tsx
@@ -1,4 +1,4 @@
-// eslint-disable-next-line react/display-name
+/* eslint-disable react/display-name */
 import React from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
