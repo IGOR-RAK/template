50636a35f9dd20b1bce161cf31b51a3389fc3b23
Igor Rak <igorrakfed@gmail.com>
2022-08-21 16:48:19 +0200

    footer init

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,22 @@
+import Navbar from "./Navbar";
+
 export default function Footer() {
-    return (
-      <footer className="footer flex bg-white text-slate-500 ">
-        <h2>Footer</h2>       
-      </footer>
-    )}
+  return (
+    <footer className="footer flex bg-sky-400 text-white mt-20 py-6 ">
+      <div className="w-4/6 mx-auto p-4 flex flex-col justify-center items-center">
+        <h2 className="mb-8 text-2xl">Kancelaria Radcy Prawnego Paweł Zdanowski</h2>
+        <div className="flex justify-center items-center">
+          <div className="px-20">           
+            <p className="mb-2">ul. Nyska 75/1, 50 – 505 Wrocław</p>
+            <p className="mb-2">Telefon: 508 170 762</p>
+            <p className="mb-2">Mail: kancelaria@czapla-zdanowska.pl</p>{" "}
+          </div>
+          <div className="px-20">           
+            <Navbar navStyle="lowercase" />
+          </div>
+        </div>
+        <div className="mt-10">Strona korzysta z plików cookies w celu realizacji usług zgodnie z Polityką Prywatności.</div>
+      </div>
+    </footer>
+  );
+}
--- components/Intro.tsx
+++ components/Intro.tsx
@@ -7,7 +7,7 @@ interface IIntro{
 
 export default function Intro({title,text}:IIntro) {
   return (
-    <section className="flex flex-col bg-white text-slate-500 w-5/6 ml-auto mr-auto dev_border">
+    <section className="flex flex-col bg-white text-slate-500 w-4/6 ml-auto mr-auto">
       <h2 className="text-3xl mb-5">{title}</h2>
       <div className="text-3xl mb-5" style={{height:"2px",borderBottom:"2px solid lightgrey"}}></div>   
       <ReactMarkdown className="markdown">{text}</ReactMarkdown>
